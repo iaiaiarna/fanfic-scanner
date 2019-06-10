72a0603aac77a2101b864c2f55e77e547aea69f5
Rebecca Turner <me@re-becca.org>
2019-06-10 13:47:31 -0700

    kind-of: Guard against being called w/undefined

--- kind-of.js
+++ kind-of.js
@@ -5,7 +5,7 @@ function kindOf (aa) {
   if (aa === null) return 'null'
   if (Array.isArray(aa)) return 'array'
   if (Buffer.isBuffer(aa)) return 'buffer'
-  if (aa instanceof Date || aa._isAMomentObject) return 'date'
+  if (aa instanceof Date || (aa && aa._isAMomentObject)) return 'date'
   if (typeof aa === 'bigint') return 'number'
   
   return typeof aa
