cdebf5333ca0ab05ab40b685693ffbc18093d18a
Rebecca Turner <me@re-becca.org>
2019-06-10 23:15:03 -0700

    scyrer: Update to new features, more readable parser

--- site/scryer.js
+++ site/scryer.js
@@ -26,12 +26,18 @@ class Scryer extends FFNet {
       items.push($(item))
     })
 
+    const $searchTitle = $('.search_controller .page-header')
+    $searchTitle.find('small').remove()
+    const searchTitle = ($searchTitle.text()||'').trim()
+    const [, searchFandom] = searchTitle.match(/^Search\s+in\s+(.*?)\s+and All Crossovers$/)
+                          || searchTitle.match(/^Search\s+in\s+(.*?)$/)
+                          || []
+
     for (let $item of items) {
       const fic = scan.addFic()
       const $footer = $item.find('div.panel-footer')
       fic.updated = moment($footer.find('time').attr('datetime')).unix()
       fic.published = moment($footer.find('strong').first().attr('title').replace(/Published: (\S+) (\S+) UTC/, '$1T$2Z')).unix()
-      fic.rawContent = $item.text().trim()
 
       const $storyLink = $item.find('a.story-link')
       fic.title = $storyLink.attr('data-story')
@@ -58,36 +64,51 @@ class Scryer extends FFNet {
       const footer = $footer.text().replace(/\s+/g, ' ').trim()
       const info = this.parseSearchLine(footer)
       const {xover, rating, words, reviews, favs, follows, chapterCount, status} = info
-      fic.summary = $summary.text().trim()
+      fic.summary = $summary.text().trim().replace(/</g, '&lt;')
       fic.chapterCount = chapterCount
       fic.words = words
+      fic.rating = this.ratingMap(rating)
       fic.stats.reviews = reviews || 0
       fic.stats.favs = favs || 0
       fic.stats.follows = follows || 0
+      if (searchFandom) fic.tags.push(`fandom:${searchFandom}`)
       if (xover) fic.tags.push(`fandom:${xover}`)
       fic.tags.push.apply(fic.tags, []
         .concat(info.genre.map(_ => `genre:${_}`))
-        .concat([`rating:${rating}`])
         .concat(info.characters.map(_ => `character:${_}`))
         .concat(info.pairing.map(_ => `ship:${_.join('/')}`))
-        .concat(labels.map(_ => `freeform:${_}`)))
+        .concat(labels.map(_ => `scryer:${_}`)))
       if (info.status === 'Complete') {
-        if (info.chapterCount <= 1) {
-          fic.tags.push('status:one-shot')
-        } else {
-          fic.tags.push('status:complete')
-        }
+        fic.status = 'complete'
+        fic.maxChapterCount = fic.chapterCount
+      } else if (info.status === 'In-Progress') {
+        fic.status = 'in-progress'
       }
     }
     return scan
   }
 
   parseSearchLine (status) {
-    let matched = status.match(qr`^((?<xover>.*?) - )?(?<status>.*?) - (?<rating>.*?) - (?:(?<genres>${this.mGenres}) )?- (?:(?<chars>.*?) )?- (?<words>${this.mNum}) words - (?<chapters>${this.mNum}) chapters? - (?<reviews>${this.mNum}) reviews? - (?<favs>${this.mNum}) favorites? - (?<follows>${this.mNum}) follows?$`)
+    const mNum = this.mNum()
+    const mGenres = this.mGenres()
+    let matched = status.match(qr.join('', [
+      qr`^`,
+      qr`(?:(?<xover>.*?) - )?`,
+      qr`(?<status>In-Progress|Complete) - `,
+      qr`(?<rating>..?)`,
+      qr`(?: - (?<genres>${mGenres}))?`,
+      qr`(?: - (?<charship>.*?))?`,
+      qr` - (?<words>${mNum}) words?`,
+      qr` - (?<chapters>${mNum}) chapters?`,
+      qr` - (?<reviews>${mNum}) reviews?`,
+      qr` - (?<favorites>${mNum}) favorites?`,
+      qr` - (?<follows>${mNum}) follows?`,
+      qr`$`
+    ]))
     if (!matched) throw new Error('Unparseable: »' + status + '«')
     const info = matched.groups
 
-    let cp = info.chars || ''
+    let cp = info.charship || ''
     let characters = []
     let pairing = []
     if (/<.+>/.test(cp)) {
