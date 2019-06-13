dea4fcb8801f36e3677066c15e9f251b7c39268a
Rebecca Turner <me@re-becca.org>
2019-06-12 23:14:45 -0700

    scanner: Only update lastseen & lastscan if they have a value

--- scanner.js
+++ scanner.js
@@ -258,8 +258,8 @@ console.log('importing', scan.dbfile)
     for await (let fic of fun(fs.createReadStream(scan.dbfile)).ndjson()) {
       if (fic.SOURCE) {
         await Promise.all([
-          scan.data.setLastSeen(fic.lastseen),
-          scan.data.setLastScan(fic.lastscan)
+          fic.lastseen && scan.data.setLastSeen(fic.lastseen),
+          fic.lastscan && scan.data.setLastScan(fic.lastscan)
         ])
       } else {
         await scan.data.replace(new Fic(scan.engine).fromJSON(fic))
